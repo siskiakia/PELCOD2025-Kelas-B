af15d68ecf925e1e380becfdcd7949b407c84b09
siskiakia <siskiapplg@gmail.com>
2025-09-13 23:32:31 +0700

    PELCOD2025-Kelas-B

---
+++ Siskia.py
@@ -0,0 +1,32 @@
+name = "Siskiatun Nafisah"
+nim = 250441100034
+semester = 1
+
+print("=====BIODATA MAHASISWA=====")
+print("Halo, nama saya:"+ name + "\n")
+print(f"NiM saya:{nim}\n")
+print("Saya berkuliah di semester  : {}" .format(semester))
+
+
+a = 10 
+b = 5
+c = a + b 
+d = a - b 
+e = a * b 
+f = a / b 
+g = a % b 
+
+print("Hasil Penjumlahan:" , c)
+print("Hasil Pengurangan:" , d)
+print("Hasil Perkalian:" , e)
+print("Hasil Pembagian:" , f)
+print("Hasil Sisan bagi:" , g)
+
+nama = input("Masukan nama Anda: ")
+umur = input("Masukan umur Anda: ")
+buah = input("Masukan buah Favorit Anda: ")
+
+print("Nama saya adalah " + nama)
+print("Umur saya adalah " + umur + "Tahun")
+print("Buah favorit saya adalah " + buah)
+
